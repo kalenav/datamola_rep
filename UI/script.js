75db86ceec767d055fb7c88efe7ed060a696e33f
kosta <konstantin.loktev.02@mail.ru>
2022-03-12 22:30:00 +0300

    getTweets method

--- UI/script.js
+++ UI/script.js
@@ -289,3 +289,21 @@ const tweets = [
         comments: []
     }
 ]
+
+(function () {
+
+    let user;
+
+    function getTweets(skip, top, filterConfig) {
+        skip = skip ?? 0;
+        top = top ?? 10;
+        let result = tweets;
+        if(filterConfig.author) result = result.filter((tweet) => tweet.author.includes(filterConfig.author));
+        if(filterConfig.dateFrom) result = result.filter((tweet) => tweet.createdAt >= filterConfig.dateFrom);
+        if(filterConfig.dateTo) result = result.filter((tweet) => tweet.createdAt <= filterConfig.dateTo);
+        if(filterConfig.hashtags) result = result.filter((tweet) => filterConfig.hashtags.every((hashtag) => tweet.text.includes(`#${hashtag}`)));
+        if(filterConfig.text || filterConfig.text === "") result = result.filter((tweet) => tweet.text.includes(filterConfig.text));
+        return result.slice(skip, skip + top);
+    }
+
+})();
