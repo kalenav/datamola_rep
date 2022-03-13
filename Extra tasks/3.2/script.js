21497b88391078a20b76737da643b734564d4800
kosta <konstantin.loktev.02@mail.ru>
2022-03-13 13:36:08 +0300

    greatestProfit function + 2 test calls

--- Extra tasks/3.2/script.js
+++ Extra tasks/3.2/script.js
@@ -16,28 +16,59 @@ function greatestProfit(array) {
     const allPossibleAuxiliaryArrays = [new Array(array.length).fill(0)];
     let auxiliary = new Array(array.length).fill(0);
 
+    // функция ниже пройдётся по всем возможным правильным способам заполнения 
+    // вспомогательного массива. так как совершенно очевидно, что ради достижения
+    // максимальной прибыли за любой покупкой должна следовать продажа, т.е. вспомогательный
+    // массив не может содержать отдельно единицу или двойку, то логичным будет утверждение,
+    // что цифры 1 и 2 всегда должны следовать парами и разделяться только нулями, если
+    // они вообще будут разделяться вообще. в первой своей итерации функция переберёт все
+    // возможные варианты расположения первой пары цифр 1 и 2. для массива
+    // длиной, например, 4, это будут следующие варианты:
+    // [1, 2, 0, 0], [1, 0, 2, 0], [1, 0, 0, 2], [0, 1, 2, 0], [0, 1, 0, 2], [0, 0, 1, 2]
+    // второй своей итерацией функция аналогичным образом переберёт 
+    // все возможные варианты расположения второй пары цифр 1 и 2, т.е. той пары, 
+    // которая идёт после первой цифры 2. таким образом, функция "разбивает"
+    // вспомогательный массив на подмассивы и перебирает возможные варианты заполнения
+    // этих подмассивов, после чего процесс повторяется рекурсивно до тех пор,
+    // пока на очередную пару цифр 1 и 2 просто не останется места
+
     function insertPairs(leftBound) {
-        if(leftBound >= auxiliary.length - 2) {
-            allPossibleAuxiliaryArrays.push(auxiliary.slice())
+        if(leftBound >= auxiliary.length - 2) { // условие выхода - больше некуда вставлять пару цифр 1 и 2
+            allPossibleAuxiliaryArrays.push(auxiliary.slice()) // запомнили копию текущего варианта заполнения массива
             return;
         }
         for(let currOnePosition = leftBound + 1; currOnePosition < auxiliary.length - 1; currOnePosition++) {
             auxiliary[currOnePosition] = 1;
             for(let currTwoPosition = currOnePosition + 1; currTwoPosition < auxiliary.length; currTwoPosition++) {
-                auxiliary[currTwoPosition] = 2;
-                insertPairs(currTwoPosition);
+                auxiliary[currTwoPosition] = 2; // положили по нужным индексам 1 и 2,..
+                insertPairs(currTwoPosition);   // и вызвали функцию от "подмассива"
+
+                // после того, как строка выше выполнилась, т.е. для текущего
+                // уровня вложенности и текущего расположения цифр 1 и 2
+                // были обработаны все возможные правильные варианты заполнения
+                // вспомогательного массива, нужно убрать двойку с позиции,
+                // которая была обработана
                 auxiliary[currTwoPosition] = 0;
             }
-            for(let i = currOnePosition; i < auxiliary.length; i++) auxiliary[i] = 0;
+
+            // после обработки текущей позиции единицы чистим её и всё, что идёт после неё
+
+            for(let i = currOnePosition; i < auxiliary.length; i++) auxiliary[i] = 0; 
         }
     }
 
-    debugger;
     insertPairs(-1);
 
-    let greatestProfit = 0;
-    console.log(allPossibleAuxiliaryArrays);
+    return allPossibleAuxiliaryArrays.reduce((r, currArray) => {
+        let currProfit = 0;
+        for(let i = 0; i < currArray.length; i++) {
+            if(currArray[i] === 1) currProfit -= array[i];
+            if(currArray[i] === 2) currProfit += array[i];
+        }
+        if(currProfit > r) return currProfit;
+        else return r;
+    }, 0);
 }
 
-// greatestProfit(arr1);
-// greatestProfit(arr2);
+greatestProfit(arr1);
+greatestProfit(arr2);
