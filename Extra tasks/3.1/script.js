1b80da812fcb9a6964183f033b5d9abbeedc1efc
kosta <konstantin.loktev.02@mail.ru>
2022-03-16 17:11:48 +0300

    extra task 3.1 fix - currRightBound now starts at currLeftBound instead of 0

--- Extra tasks/3.1/script.js
+++ Extra tasks/3.1/script.js
@@ -1,15 +1,16 @@
 const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const arr2 = [-1, -2];
 
 function largestContinuousSubarraySum(array) {
     let sum;
     for(let currLeftBound = 0; currLeftBound < array.length; currLeftBound++) {
-        for(let currRightBound = 0; currRightBound < array.length; currRightBound++) {
+        for(let currRightBound = currLeftBound; currRightBound < array.length; currRightBound++) {
             let currSum = array.slice(currLeftBound, currRightBound + 1).reduce(((r, v) => r += v), 0);
-            if(!sum) sum = currSum;
-            else if(currSum > sum) sum = currSum;
+            if((!sum && sum != 0) || currSum > sum) sum = currSum;
         }
     }
     return sum;
 }
 
 console.log(largestContinuousSubarraySum(arr));
+console.log(largestContinuousSubarraySum(arr2));
